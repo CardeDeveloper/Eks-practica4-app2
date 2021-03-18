083c3b6ba526c4f4e6a0878d3f47c99a1f252986
Oscar Angel Cardenas Aguilar <oscar.cardenas@oscar.cardenas>
2021-03-17 19:05:09 -0600

    first commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Esta es la aplicación 2' });
+});
+
+router.get('/estoyvivo', function(req, res, next) {
+  res.json({"status":"si estoy vivo"});
+});
+module.exports = router;
